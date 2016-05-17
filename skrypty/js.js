3eacaa2945688b276ff9cc28ad1fb1a44cd59330
CandyTrouble <lisbeth@poczta.onet.pl>
2016-05-17 09:51:25 +0200

    ♥

--- skrypty/js.js
+++ skrypty/js.js
@@ -1,3 +1,7 @@
 function getIP(json) {
-	document.write("My public IP address is: ", json.ip);
+	//document.write("My public IP address is: ", json.ip);
+	a=document.getElementsByName('pre');
+	b=getElementById('zupa');
+	zupa.value=a;
+	document.getElementById("zupa2").src = "https://api.ipify.org/";
 }
